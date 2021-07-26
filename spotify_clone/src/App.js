3dec8fe6dddf16818744d25eebe66f5d0778e8e2
Garima Sharma <sparsh.14.99@gmail.com>
2021-07-26 13:55:44 +0530

    second commit

--- spotify_clone/src/App.js
+++ spotify_clone/src/App.js
@@ -1,22 +1,42 @@
 import './App.css';
 import Login from './Login';
-import React,{useEffect, useState} from 'react';
-import {getTokenFromUrl} from './Spotify';
+import React, { useEffect, useState } from 'react';
+import { getTokenFromUrl } from './Spotify';
 import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+import { useDataLayerValue } from './DataLayer';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  const [ token , setToken] = useState(null);
-  useEffect(()=>{
-    const hash=getTokenFromUrl();
-    window.location.hash="";
+  const [token, setToken] = useState(null);
+  const [{ user }, dispatch] = useDataLayerValue();
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
     const _token = hash.access_token;
-    if(_token){
+    if (_token) {
       setToken(_token);
+
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        })
+      })
     }
-    console.log("I have a token -> ",token)
-  });
+    console.log("I have a token -> ", token);
+
+  }, []);
+  console.log("Person", user);
+
+
+
   return (
-    <div className="app">{ token ? ( <h1>I am Login...</h1>) : (<Login/>)}</div>
+    <div className="app">{token ? (<Player />) : (<Login />)}</div>
   );
 }
 
--- spotify_clone/src/Login.js
+++ spotify_clone/src/Login.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Login.css';
-import {loginUrl} from "./Spotify";
+import { loginUrl } from "./Spotify";
 
-function Login(){
-    return(
+function Login() {
+    return (
         <div className="login">
-            <img src ="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt=""></img>
+            <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt=""></img>
             <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
         </div>
     );
